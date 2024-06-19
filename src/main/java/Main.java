5466db389c45bc49084ec9dc3fb938617d60d4b8
Strongman15 <161622418+Strongman15@users.noreply.github.com>
2024-06-19 14:05:42 +0000

    Zaliczenie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,27 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner input = new Scanner(System.in);
+    System.out.println("Podaj ilość liczb do posortowania: ");
+    int n = input.nextInt();
+    int[] tab = new int[n];
+    for(int i=0; i<n; i++){
+      System.out.println("Podaj liczbę: ");
+      tab[i] = input.nextInt();
+    }
+    for(int i=0; i<n; i++){
+      for(int j=0; j<n-1; j++){
+        if(tab[j]<tab[j+1]){
+          int temp = tab[j];
+          tab[j] = tab[j+1];
+          tab[j+1] = temp;
+        }
+      }
+    }
+    for(int i=0; i<n; i++){
+      System.out.println(tab[i]);
+    }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+  }
 }
+
